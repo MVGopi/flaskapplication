118e93e3880c552908cccab8b688f16645815923
venkata gopi <venkatagopi894@gmail.com>
2019-01-10 11:07:12 +0530

    flask files adding

--- 10c.py
+++
@@ -1,7 +0,0 @@
-gcd=lambda m,n:m if not n else gcd(n,m%n)
-print(gcd(5,25))
-lcm=lambda m,n:m if not n else m*n// gcd(n,m%n)
-print(lcm(2,3))
-print("my commit")
-print("gopi")
-print("my name venkatesh")
